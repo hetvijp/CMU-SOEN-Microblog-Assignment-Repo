747bedd1d2b0e0ec5748bd4ba892148d7e893fcd
Hetvi Patel <hetvijp99@gmail.com>
2022-06-30 21:46:10 -0400

    Changes in routes.py

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -21,6 +21,9 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
+        if user is None:
+            user = User.query.filter_by(email=form.username.data).first()
+            
         if user is None or not user.check_password(form.password.data):
             flash(_('Invalid username or password'))
             return redirect(url_for('auth.login'))
