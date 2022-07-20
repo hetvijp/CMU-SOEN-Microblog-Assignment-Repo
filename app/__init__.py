1cba2978a257f1a8c72a7c2cfb04138a27fe3555
Hetvi Patel <hetvijp99@gmail.com>
2022-07-19 21:46:02 -0400

    add flake8 file

--- app/__init__.py
+++ app/__init__.py
@@ -94,3 +94,6 @@ def create_app(config_class=Config):
 @babel.localeselector
 def get_locale():
     return request.accept_languages.best_match(current_app.config['LANGUAGES'])
+
+
+from app import models
