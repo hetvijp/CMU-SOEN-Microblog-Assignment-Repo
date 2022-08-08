4b8ad0ad3befa73b585ace54a04e43763d61c90d
Hetvi Patel <hetvijp99@gmail.com>
2022-08-07 21:29:31 -0400

    module-8

--- app/models.py
+++ app/models.py
@@ -4,11 +4,13 @@ from hashlib import md5
 import json
 import os
 from time import time
+from urllib import response
 from flask import current_app, url_for
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
 import redis
+import requests
 import rq
 from app import db, login
 from app.search import add_to_index, remove_from_index, query_index
@@ -125,9 +127,15 @@ class User(UserMixin, PaginatedAPIMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'http://localhost:5001/images/{}/size/{}'.format(digest, size)
-        # return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
-        #     digest, size)
+        try:
+            response = requests.get('http://localhost:5001/health-check')
+            if response.status_code == 200:
+                return 'http://localhost:5001/images/{}/size/{}'.format(digest, size)
+        except Exception as e:
+            print(e)
+            return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+                digest, size)
+            pass
 
     def follow(self, user):
         if not self.is_following(user):
